0b8804c4fc32f480928f3e437cdfbb3b3bb63460
maks <maks.marolt@gmail.com>
2025-08-28 00:21:54 +0200

    Revert "change exe from onefile" putting onefile back for simplicity This reverts commit 3309df4d6edc08be0b8269a0489c4b16d5177f48.

--- AssetChanger/_internal/UnityPy/helpers/__init__.py
+++
@@ -1,9 +0,0 @@
-from . import ArchiveStorageManager, CompressionHelper, ContainerHelper, ImportHelper, TypeTreeHelper
-
-__all__ = [
-    "ArchiveStorageManager",
-    "CompressionHelper",
-    "ImportHelper",
-    "TypeTreeHelper",
-    "ContainerHelper",
-]
--- AssetChanger/_internal/UnityPy/streams/__init__.py
+++
@@ -1,7 +0,0 @@
-from .EndianBinaryReader import EndianBinaryReader
-from .EndianBinaryWriter import EndianBinaryWriter
-
-__all__ = [
-    "EndianBinaryReader",
-    "EndianBinaryWriter",
-]
